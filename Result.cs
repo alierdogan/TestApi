a002436970f030b2bcb6e609ffcf41814ed4044f
alierdogan <n.alierdogan@hotmail.com>
2023-05-03 17:45:18 +0300

    Result eklendi

---
+++ Result.cs
@@ -0,0 +1,9 @@
+﻿namespace TestApi
+{
+    public class Result<TEntity> where TEntity : class, new()
+    {
+        public string Status { get; set; }
+        public string Error { get; set; }
+        public TEntity Data { get; set; }
+    }
+}
